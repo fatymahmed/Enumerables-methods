227fbb70c4f134de07f193201d5ac44571815fe7
fatima ahmed <fatymah001@gmail.com>
2019-08-07 20:37:08 +0300

    fix errors by stickler

--- my_methods.rb
+++ my_methods.rb
@@ -1,66 +1,67 @@
 module Enumerable
-    def my_each
+  def my_each
       # your code here
-      i = 0
-      while i<self.length
-        yield(self[i])
-        i += 1
-      end
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
     end
+  end
 
-    def my_each_with_index
+  def my_each_with_index
         # your code here
-        i = 0
-        while i<self.length
-          yield(i,self[i])
-          i += 1
-        end
+    i = 0
+    while i < self.length
+      yield(i,self[i])
+      i += 1
       end
+  end
 
-    def my_select
-        result = []
-        self.my_each{ |element| result << element if yield(element)}
-    end  
-    def my_all
-        flag = true
-        self.my_each{ |element| flag = flag && yield(element)}
-        flag
-    end  
+  def my_select
+    result = []
+    self.my_each { |element| result << element if yield(element) }
+  end  
+    
+  def my_all
+    flag = true
+    self.my_each { |element| flag = flag && yield(element) }
+    flag
+  end  
 
-    def my_any
-      flag = false
-      self.my_each{ |element| flag = flag || yield(element)}
-      flag
-    end  
+  def my_any
+    flag = false
+    self.my_each { |element| flag = flag || yield(element) }
+    flag
+  end  
 
-    def my_none
-       flag = false
-        self.my_each{ |element| flag = flag || yield(element)}
-        !flag
-    end  
+  def my_none
+    flag = false
+    self.my_each { |element| flag = flag || yield(element) }
+    !flag
+  end  
 
-    def my_count
-      count = 0   
-      self.my_each{ |element| count +=1 if yield(element)}
-      count
-   end  
+  def my_count
+    count = 0   
+    self.my_each { |element| count +=1 if yield(element) }
+    count
+  end  
 
-    def my_map(proc = nil)
-      result = []
-      self.my_each{ |element|
-        if proc
-          result <<  proc.call(element)         
-        else
-          result << yield(element)
-        end
+  def my_map(proc = nil)
+    result = []
+    self.my_each { |element|
+    if proc
+      result <<  proc.call(element)         
+    else
+      result << yield(element)
+    end
       }
-      result
-    end  
+    result
+  end  
 
   def my_inject(i)
-    res = i || 0
-    self.my_each{ |element| res = yield(res,element)}
-    res
+    result = i || 0
+    self.my_each { |element| result = yield(result,element) }
+    result
   end
 
   end
