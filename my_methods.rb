44b3c9061dcc069794310845dca5a519ebeaf3c5
fatima ahmed <fatymah001@gmail.com>
2019-08-08 21:55:27 +0300

    fix linters code

--- my_methods.rb
+++ my_methods.rb
@@ -75,32 +75,33 @@ module Enumerable #:nodoc:
     result
   end
 end
-    # def multiply_els(arr)
-    #     arr.my_inject(1){|res,ele| res*ele}
-    # end
-    # puts %w[ant bear cat].all? { |word| word.length >= 3 }
+# def multiply_els(arr)
+# arr.my_inject(1){|res,ele| res*ele}
+# end
+# puts %w[ant bear cat].all? { |word| word.length >= 3 }
 
-    #  arr=[1,2,3,4]
-    #  a1=arr.each{|element| element}
-    #  puts a1
-    # a2=arr.my_each{|element| element}
-    # puts a2
-    # arr.my_each_with_index{|element, index| puts "element #{element} at #{index} index"}
-    # puts [2,1,2,4,8].my_all { |num|  num.even?  }
+# arr=[1,2,3,4]
+# a1=arr.each{|element| element}
+# puts a1
+# a2=arr.my_each{|element| element}
+# puts a2
+# arr.my_each_with_index{|element, index| puts "element #{element} at #{index} index"}
+# puts [2,1,2,4,8].my_all { |num|  num.even?  }
 
-    #  puts [2,2,2,1,8].my_any { |num|  num.odd?  }
-    # puts [2,2,2,2,2].my_none { |num|  num.odd?  }
+# puts [2,2,2,1,8].my_any { |num|  num.odd?  }
+# puts [2,2,2,2,2].my_none { |num|  num.odd?  }
 
-    # puts [2,7,2,5,2].my_count{ |num|  num.odd?  }
-    # puts [2,2,2,5,2].my_count(2){}
+# puts [2,7,2,5,2].my_count{ |num|  num.odd?  }
+# puts [2,2,2,5,2].my_count(2){}
 
-    #  puts ["hi","hey"].my_map{|element| element.upcase}
-    # puts [1, 2, 4, 2].my_count(2)
-    # arr=[2,2,2,5,2]
-    # res=arr.my_inject(1){|res,ele| res+ele}
-    # puts res
-    # multiply_els([2,4,5])
-    # proc=Proc.new{
-    # |element|
-    # element.upcase}
-    # ["hi","hey"].my_map(proc)
+# puts ["hi","hey"].my_map{|element| element.upcase}
+# puts [1, 2, 4, 2].my_count(2)
+# arr=[2,2,2,5,2]
+# res=arr.my_inject(1){|res,ele| res+ele}
+# puts res
+# multiply_els([2,4,5])
+# proc=Proc.new{
+# |element|
+# element.upcase}
+# ["hi","hey"].my_map(proc)
+    
