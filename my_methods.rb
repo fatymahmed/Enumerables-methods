0cdff01a5e2f7ca4b8b7d69e4e1557bae7277ce3
fatima ahmed <fatymah001@gmail.com>
2019-08-08 10:48:35 +0300

    made changes according to review

--- my_methods.rb
+++ my_methods.rb
@@ -3,17 +3,19 @@
 module Enumerable #:nodoc:
   def my_each
     # your code here
+    result=[]
     i = 0
     while i < length
-      yield(self[i])
+      result<<yield(self[i])
       i += 1
     end
+    result
   end
 
   def my_each_with_index
     i = 0
     while i < length
-      yield(i, self[i])
+      yield(self[i], i)
       i += 1
     end
   end
@@ -21,68 +23,85 @@ module Enumerable #:nodoc:
   def my_select
     result = []
     my_each { |element| result << element if yield(element) }
+    return result
   end
 
   def my_all
-    flag = true
-    my_each { |element| flag &&= yield(element) }
-    flag
+    self.my_each { |item|
+      if !yield(item)
+        return false
+      end
+    }
+    return true
   end
 
   def my_any
-    flag = false
-    my_each { |element| flag ||= yield(element) }
-    flag
+    self.my_each { |item|
+      if yield(item)
+        return true
+      end
+    }
+    return false
   end
 
   def my_none
-    flag = false
-    my_each { |element| flag ||= yield(element) }
-    !flag
+    result = true
+    self.my_each{|item| result = false if yield(item)}
+    return result
   end
 
   def my_count
     count = 0
     my_each { |element| count += 1 if yield(element) }
-    count
+    return count
   end
 
-  def my_map(proc = nil)
+  def my_map(&block)
     result = []
-    result <<
-      my_each do |element|
-        if proc
-          proc.call(element)
-        else
-          yield(element)
-        end
+    unless block == nil
+      self.my_each do |item| 
+        result << block.call(item) 
       end
-    result
-  end
+    else
+      return result
+    end
+  
+    return result
+  end  
+
 
   def my_inject(initial)
     result = initial || 0
     my_each do |element|
       result = yield(result, element)
     end
-    result
+    return result
   end
 end
 # def multiply_els(arr)
 #     arr.my_inject(1){|res,ele| res*ele}
 # end
 
-#   arr=[1,2,3,4]
-#   arr.my_each{|element| puts element}
-#   arr.my_each_with_index{|index,element| puts "index: #{index} for #{element}"}
-#   [2,2,2,4,8].my_all { |num|  num.even?  }
-#   [2,2,2,4,8].my_any { |num|  num.odd?  }
-#   [2,2,2,5,7].my_none { |num|  num.odd?  }
+    # arr=[1,2,3,4]
+    # a1=arr.each{|element| element}
+    # puts a1
+    # a2=arr.my_each{|element| element}
+    # puts a2
+    # arr.my_each_with_index{|element, index| puts "element #{element} at #{index} index"}
+    # puts [2,1,2,4,8].my_all { |num|  num.even?  }
+
+    # puts [2,2,2,1,8].my_any { |num|  num.odd?  }
+    # puts [2,2,2,2,2].my_none { |num|  num.odd?  }
 
 #   [2,2,2,5,2].my_count{ |num|  num.odd?  }
-#   ["hi","hey"].my_map{|element| element.upcase}
-#   [2,2,2,5,2].my_inject(1){|res,ele| res+ele}
-#   multiply_els([2,4,5])
+
+  # pc=["hi","hey"].my_map{|element| element.upcase}
+  # puts pc
+
+  #   arr=[2,2,2,5,2]
+#   res=arr.my_inject(1){|res,ele| res+ele}
+#   puts res
+# #   multiply_els([2,4,5])
 #   proc=Proc.new{
 #     |element|
 #     element.upcase
