0ad2a31c9c2c5b2900a3277f8b3be3b4ec99fd84
Urchmaney <kingsobino@gmail.com>
2019-08-09 21:00:09 +0000

    Finish implementing the test case with rspec.

--- spec/my_methods_spec.rb
+++ spec/my_methods_spec.rb
@@ -7,18 +7,28 @@ RSpec.describe Enumerable do
 		end
 	end
 	describe "#my_all" do
-			it "returns true if condition holds for all elements ,otherwise returns false" do
-				expect([2,1,2,4,8].my_all { |num|  num.even?  }).to eql(false)
-			end
-		end
-		describe "#my_any" do
-			it "returns false if condition doesn't hold for all elements ,otherwise returns true" do
-				expect([2,1,2,4,8].my_any { |num|  num.even?  }).to eql(true)
-			end
-		end
-		describe "#my_none" do
-			it "returns false if condition hold for any elements ,otherwise returns true" do
-				expect([2,1,2,4,8].my_none { |num|  num.even?  }).to eql(false)
-			end
-		end
+    it "returns true if condition holds for all elements ,otherwise returns false" do
+      expect([2,1,2,4,8].my_all { |num|  num.even?  }).to eql(false)
+    end
   end
+  describe "#my_any" do
+    it "returns false if condition doesn't hold for all elements ,otherwise returns true" do
+      expect([2,1,2,4,8].my_any { |num|  num.even?  }).to eql(true)
+    end
+  end
+  describe "#my_none" do
+    it "returns false if condition hold for any elements ,otherwise returns true" do
+      expect([2,1,2,4,8].my_none { |num|  num.even?  }).to eql(false)
+    end
+  end
+  describe "#my_count" do
+    it "returns the frequency of element in array.return the array size if parameter is not given." do
+      expect([2,2,2,5,2].my_count(2){}).to eql(4)
+    end
+  end
+  describe "#my_inject" do
+    it "returns the accumulated result of operation performed on each element in the array with initial value given as parameter." do
+      expect([2,2,2,5,2].my_inject(1){|res,ele| res+ele}).to eql(14)
+    end
+  end
+end
